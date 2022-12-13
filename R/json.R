13ab5f2ffb59f245e000da4a3d65697dd59f55d6
NadjaBramkamp <n.bramkamp@web.de>
2022-12-13 11:11:29 +0100

    comments for JSON conversion

--- R/json.R
+++ R/json.R
@@ -3,30 +3,30 @@
 #' Functions for converting the different aspects to JSON following the CX data structure definition
 #' (see NDEx documentation: \url{https://home.ndexbio.org/data-model/}).
 #'
-#' For converting \link[RCX:compareNetworks]{RCX objects} to JSON, each aspect is processed by a generic function for its
-#' aspect class. Those functions return a character only containing the JSON of this aspect, which is then
-#' combined by [RCX::toCX] to be a valid CX data structure.
+#' For converting \link[RCX:NetworkDifferencesAspect]{RCX objects} to JSON, each aspect is processed by a generic function for its
+#' aspect class. Those functions return a character only containing the JSON of this aspect.
 #'
 #' To support the conversion for non-standard or own-defined aspects, generic functions for those aspect classes
 #' have to be implemented.
 #'
-#' @param aspect [NetworkDifferences] aspect
+#' @param aspect [RCX-object] with NetworkDifferences-Aspect
 #' @param verbose logical; whether to print what is happening
 #' @param ... additional parameters, that might needed for extending
 #'
 #' @return character; JSON of an aspect
 #' @export
-#' @importFrom RCX rcxToJson, .convert2json.logical, .convert2json.data.frame
+#' @importFrom RCX rcxToJson, .addClass
 #'
 #' @seealso [RCX::toCX], [RCX::writeCX], [RCX::jsonToRCX], [RCX::readCX]
-#'
-#' @example inst/examples/toJson.R
 rcxToJson.NetworkDifferencesAspect = function(aspect, verbose=FALSE, ...){
+    ## Use the existing functions from the RCX package to convert to nodes, nodeAttributes, edges, edgeAttributes to JSON
     nodes <- RCX:::rcxToJson.NodesAspect(aspect$nodes)
     nodeAttributes <- RCX:::rcxToJson.NodeAttributesAspect(aspect$nodeAttributes)
     edges <- RCX:::rcxToJson.EdgesAspect(aspect$edges)
     edgeAttributes <- RCX:::rcxToJson.EdgeAttributesAspect(aspect$edgeAttributes)
+    ## Convert the networkDifference-Aspect to JSON
     networkDifferences <- rcxToJson.NetworkDifferences(aspect$networkDifferences)
+    ## Concatenate the different string to the result string
     result <- c(nodes, nodeAttributes, edges, edgeAttributes, networkDifferences)
     
     result = c(result, '{"status":[{"error":"","success":true}]}')
@@ -34,6 +34,7 @@ rcxToJson.NetworkDifferencesAspect = function(aspect, verbose=FALSE, ...){
     result = paste0(result, collapse = ",")
     result = paste0("[",result,"]")
     
+    ## Add class
     class(result) = "json"
     RCX:::.addClass(result) = "CX"
     
@@ -42,17 +43,9 @@ rcxToJson.NetworkDifferencesAspect = function(aspect, verbose=FALSE, ...){
 
 #' Convert parsed JSON aspects to RCX
 #'
-#' Functions to handle parsed JSON for the [MetaRelSubNetVis] aspect.
+#' Functions to handle parsed JSON for the [NetworkDifferences] aspect.
 #'
 #' These functions will be used in [RCX::processCX] to process the JSON data for every aspect.
-#' Each aspect is accessible in the CX-JSON by a particular accession name (i.e. its aspect name; see NDEx documentation:
-#' \url{https://home.ndexbio.org/data-model/}).
-#' This name is used as class to handle different aspects by method dispatch.
-#' This simplifies the extension of RCX for non-standard or self-defined aspects.
-#'
-#' The CX-JSON is parsed to R data types using the [jsonlite] package as follows:
-#'
-#' `jsonlite::fromJSON(cx, simplifyVector = FALSE)`
 #'
 #' This results in a list of lists (of lists...) to avoid automatic data type conversions, which affect the correctness and
 #' usability of the data. Simplified JSON data for example [NodeAttributes] would be coerced into a data.frame,
@@ -70,29 +63,38 @@ rcxToJson.NetworkDifferencesAspect = function(aspect, verbose=FALSE, ...){
 #'
 #' @return created aspect or `NULL`
 #' @export
-#' @importFrom RCX jsonToRCX
+#' @importFrom RCX jsonToRCX, createRCX, updateNodeAttributes, updateEdgeAttributes
 #'
 #' @seealso [RCX::rcxToJson], [RCX::toCX], [RCX::readCX], [RCX::writeCX]
-#'
-#' @example inst/examples/fromJson.R
-jsonToRCX.networkDifferencesAspect = function(jsonData, verbose){
+jsonToRCX.networkDifferencesAspect = function(jsonData, verbose = FALSE){
     if(verbose) cat("Parsing NetworkDifferences...")
+    ## Reconvert each aspect
     nodes = RCX:::jsonToRCX.nodes(jsonData[[1]], verbose = TRUE)
     nodeAttributes = RCX:::jsonToRCX.nodeAttributes(jsonData[[2]], verbose = TRUE)
     edges = RCX:::jsonToRCX.edges(jsonData[[3]], verbose = TRUE)
     edgeAttributes = RCX:::jsonToRCX.edgeAttributes(jsonData[[4]], verbose = TRUE)
+    ## Create result RCX object
     rcx = RCX:::createRCX(nodes = nodes, edges = edges)
-    rcx <- updateNodeAttributes(rcx, nodeAttributes)
-    rcx <- updateEdgeAttributes(rcx, edgeAttributes)
-    networkDifferences <- jsonToRCX.networkDifferences(jsonData[[5]], verbose = TRUE)
-    rcx$networkDifferences <- networkDifferences
+    rcx <- RCX:::updateNodeAttributes(rcx, nodeAttributes)
+    rcx <- RCX:::updateEdgeAttributes(rcx, edgeAttributes)
+    rcx$networkDifferences <- jsonToRCX.networkDifferences(jsonData[[5]], verbose = TRUE)
     return(rcx)
 }
 
-rcxToJson.NetworkDifferences = function(aspect, verbose = FALSE, ...) {
-    if(verbose) cat("Convert NetworkDifferences to JSON...")
 
+#' Helper function to convert the networkDifferences-aspect
+#'
+#' @param aspect [NetworkDifferences] aspect
+#' @param verbose logical; whether to print what is happening
+#'
+#' @return character; JSON of the [NetworkDifferences] aspect
+#' 
+#' @importFrom RCX .convert2json.logical, .convert2json.data.frame
+rcxToJson.NetworkDifferences = function(aspect, verbose = FALSE) {
+    if(verbose) cat("Convert NetworkDifferences to JSON...")
+    ## Convert MatchByName
     aspect$matchByName = RCX:::.convert2json.logical(aspect$matchByName)
+    ## Convert Nodes
     if (aspect$matchByName == "\"true\"") {
         map = c(id="@id",
                 name="n",
@@ -114,7 +116,7 @@ rcxToJson.NetworkDifferences = function(aspect, verbose = FALSE, ...) {
                 belongsToRight="btr")
     }
     aspect$nodes = RCX:::.convert2json.data.frame(RCX:::.renameDF(aspect$nodes, map), skipNa = TRUE)
-
+    ## Convert NodeAttributes
     map = c(propertyOf="po",
             name="n",
             belongsToLeft="btl",
@@ -126,7 +128,7 @@ rcxToJson.NetworkDifferences = function(aspect, verbose = FALSE, ...) {
             valueLeft="vl",
             valueRight="vr")
     aspect$nodeAttributes = RCX:::.convert2json.data.frame(RCX:::.renameDF(aspect$nodeAttributes, map), skipNa = TRUE)
-
+    ## Convert edges
     map = c(id="@id",
             name="n",
             source="s",
@@ -137,7 +139,7 @@ rcxToJson.NetworkDifferences = function(aspect, verbose = FALSE, ...) {
             belongsToLeft="btl",
             belongsToRight="btr")
     aspect$edges = RCX:::.convert2json.data.frame(RCX:::.renameDF(aspect$edges, map), skipNa = TRUE)
-
+    ## Convert edgeAttributes
     map = c(propertyOf="po",
             name="n",
             belongsToLeft="btl",
@@ -149,7 +151,7 @@ rcxToJson.NetworkDifferences = function(aspect, verbose = FALSE, ...) {
             valueLeft="vl",
             valueRight="vr")
     aspect$edgeAttributes = RCX:::.convert2json.data.frame(RCX:::.renameDF(aspect$edgeAttributes, map), skipNa = TRUE)
-
+    ## Convert nodeAttributes
     map = c(name="n",
             belongsToLeft="btl",
             belongsToRight="btr",
@@ -160,7 +162,7 @@ rcxToJson.NetworkDifferences = function(aspect, verbose = FALSE, ...) {
             valueLeft="vl",
             valueRight="vr")
     aspect$networkAttributes = RCX:::.convert2json.data.frame(RCX:::.renameDF(aspect$networkAttributes, map), skipNa = TRUE)
-
+    ## Create result
     json = paste0('{"matchByName":',aspect$matchByName,'},{"nodes":',aspect$nodes,'},{"nodeAttributes":',aspect$nodeAttributes,
                   '},{"edges":',aspect$edges,'},{"edgeAttributes":',aspect$edgeAttributes,'},{"networkAttributes":',aspect$networkAttributes,'}')
     json = paste0('{"networkDifferences":[',json,']}')
@@ -168,10 +170,15 @@ rcxToJson.NetworkDifferences = function(aspect, verbose = FALSE, ...) {
     return(json)
 }
 
-
-#' @rdname jsonToRCX
-#' @export
-jsonToRCX.networkDifferences = function(jsonData, verbose){
+#' Helper function to convert the JSON data back to rcx
+#'
+#' @param jsonData nested list from parsed JSON
+#' @param verbose logical; whether to print what is happening
+#'
+#' @return created [NetworkDifferences] aspect or `NULL`
+#' 
+#' @importFrom RCX .jsonV
+jsonToRCX.networkDifferences = function(jsonData, verbose = FALSE){
     if(verbose) cat("Parsing networkDifferences...")
     ## matchByName
     data = jsonData$networkDifferences[[1]]$matchByName
@@ -266,6 +273,7 @@ jsonToRCX.networkDifferences = function(jsonData, verbose){
                                    dataTypeLeft, dataTypeRight, isListLeft, isListRight,
                                    valueLeft, valueRight)
     if(verbose) cat("create aspect...")
+    ## Create result
     result = list("matchByName" = matchByName, "nodes" = nodes, "nodeAttributes" = nodeAttributes,
                   "edges" = edges, "edgeAttributes" = edgeAttributes, "networkAttributes" = networkAttributes)
     class(result) = c(class(result), "NetworkDifferencesAspect")
